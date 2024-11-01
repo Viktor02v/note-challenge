874b4ab2cf6223f11f8935bef1db7fa574d9dec9
Viktor Petruk <damnnn261@gmail.com>
2024-11-01 08:02:26 +0200

    add some styling

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@ import { ref, onMounted, watch } from 'vue';
 // QuillEditor
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
-import { WatchDirectoryFlags } from 'typescript';
 
 // States for the QuillEditor
-const editorContent = ref('');
+const editorContent = ref((JSON.parse(localStorage.getItem('editorContent') || '')));
 const timeout = ref(null);
 const editor = ref();
-
 
 // States for the Preview
 const comments = ref(JSON.parse(localStorage.getItem('comments') || '[]'));
@@ -24,14 +22,21 @@ const userInfo = (event) => {
 
 // Add Comment
 const addComment = () => {
-	const comment = {
-		id: nextId,
-		name: userName.value,
-		content: editorContent.value,
+	if (editorContent.value.length < 5) {
+		alert('You need to type something in')
+		return
+	} else if (!userName.value) {
+		alert('You need to type your Nick Name')
+		return
+	} else {
+		const comment = {
+			id: nextId,
+			name: userName.value,
+			content: editorContent.value,
+		}
+		comments.value.push(comment);
+		nextId++;
 	}
-	comments.value.push(comment);
-	nextId++;
-	console.log(comments.value);
 };
 
 const deleteComment = (id) => {
@@ -40,7 +45,6 @@ const deleteComment = (id) => {
 		comments.value.splice(index, 1);
 	};
 };
-
 
 
 // Clear the content of the Editor
@@ -68,60 +72,62 @@ watch(comments, saveComments, { deep: true });
 
 // Parse the content of the Editor 
 onMounted(() => {
-	const savedContent = localStorage.getItem('editorContent');
+	const savedContent = JSON.parse(localStorage.getItem('editorContent'));
 	if (savedContent) {
-		editorContent.value = JSON.parse(savedContent);
+		editorContent.value = savedContent;
 	};
 	const savedComments = JSON.parse(localStorage.getItem('comments'));
 	if (savedComments) {
 		comments.value = savedComments;
 	}
 });
+
+console.log(editorContent.value.length)
 </script>
 
 <template>
-	<div class="w-full h-full container py-20 flex gap-10 ">
+	<div class="w-full h-full md:flex container py-20 gap-10 ">
+
 		<!-- Editor Content -->
-		<div class="w-[50vw] flex flex-col h-full">
-			<div class="mb-5 gap-10 flex w-full">
-				<button @click="clearContent" class="p-2 rounded-md w-full bg-gray-400 text-white">
+		<div class="w-full md:w-[40vw] h-full mb-10 md:mb-0 flex flex-col ">
+
+			<div class="flex mb-5 w-[20vw] md:w-[10vw] gap-3 flex-col">
+				<label for="name">Nick Name</label>
+				<input class="p-2 rounded-md border " @input="userInfo" name="name" type="text">
+			</div>
+
+			<QuillEditor class="w-full h-[10rem] overflow-hidden" @update:content="saveEditorContent"
+				v-model:content="editorContent" content-type="text" theme="snow" ref="editor" />
+
+			<div class="mt-5 gap-10 flex w-full">
+
+				<button @click="clearContent"
+					class="p-2 rounded-md hover:bg-gray-400 hover:transition-all hover:duration-200 w-full bg-gray-300 text-white">
 					Clear Content
 				</button>
-				<button @click="addComment" class="p-2 rounded-md w-full bg-gray-400 text-white">
+
+				<button @click="addComment"
+					class="p-2 rounded-md hover:bg-gray-400 hover:transition-all hover:duration-200 w-full bg-gray-300 text-white">
 					Add Comment
 				</button>
-			</div>
-			<div class="">
-				<div class="flex w-[50%] flex-col">
-					<label for="name">Name</label>
-					<input @input="userInfo" name="name" type="text">
-				</div>
-				<QuillEditor @update:content="saveEditorContent" v-model:content="editorContent" content-type="html"
-					theme="snow" ref="editor" />
 			</div>
 		</div>
 
 		<!-- Preview Section -->
-		<div class="flex w-full items-center flex-col gap-5">
+		<div class="flex w-[90vw] items-center flex-col gap-5">
 			<h2 class="text-2xl font-bold">Preview</h2>
-			<!-- Comment Section -->
-			<div id="comment" class="w-full">
-				<div v-for="comment in comments" :key="comment.id" class="">
-					<!-- Comment in Comments -->
-					<div class="border flex items-center w-full justify-between container rounded-md bg-gray-200 text-white">
 
-						<div>{{ comment.name }}</div>
-
-						<div v-html="comment.content" class="text-black text-[1.25rem]"></div>
-
-						<button type="button" @click="deleteComment(comment.id)"> Delete </button>
-					</div>
+			<div v-for="comment in comments" :key="comment.id" class="w-full min-h-[10vh]  flex flex-col">
+				<!-- Comment in Comments -->
+				<div class="border relative py-10 w-full container rounded-md bg-gray-200 text-white">
+					<div class="absolute p-0.5 bg-white rounded-md text-black top-[0.5rem] left-[1rem]">{{ comment.name }}</div>
+					<div v-text="comment.content" class="text-black w-full text-[1.25rem]"></div>
+					<button class="absolute bottom-1 right-[1rem] p-0.5 bg-white rounded-md text-black" type="button" @click="deleteComment(comment.id)"> Delete
+					</button>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
